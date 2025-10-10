d6954c13fe7bb109ec5b6543ad9b44d975091b79
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-10-10 12:07:32 +0200

    configuration ExceptionListener

--- src/EventListener/ExcpetionListener.php
+++ src/EventListener/ExcpetionListener.php
@@ -3,13 +3,29 @@
 namespace App\EventListener;
 
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 
 final class ExcpetionListener
 {
     #[AsEventListener]
     public function onExceptionEvent(ExceptionEvent $event): void
     {
-        // ...
+        $exception = $event->getThrowable();
+        
+        if($exception instanceof HttpException){
+            $data = [
+                'status' => $exception->getStatusCode(),
+                'message' => $exception->getMessage(),
+            ]; 
+        }else{
+            $data = [
+                'status' => 500, 
+                'message' => $exception->getMessage()
+            ];
+        }
+        $event->setResponse(new JsonResponse($data));
     }
 }
